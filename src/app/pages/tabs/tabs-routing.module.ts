d203f609e7cd815d473437e0fde98e920a749937
Subela <olindasuely@yahoo.com.br>
2020-05-23 19:00:10 +0000

    Implement Camera function

--- src/app/pages/tabs/tabs-routing.module.ts
+++ src/app/pages/tabs/tabs-routing.module.ts
@@ -48,15 +48,11 @@ const routes: Routes = [
                 ]
             },
             {
-                path: 'deaths',
+                path: 'camera',
                 children: [
                     {
                         path: '',
-                        loadChildren: () => import('../deaths/deaths.module').then(m => m.DeathsPageModule)
-                    },
-                    {
-                        path: ':id',
-                        loadChildren: () => import('../death-details/death-details.module').then(m => m.DeathDetailsPageModule)
+                        loadChildren: () => import('../camera/camera.module').then(m => m.CameraPageModule)
                     }
                 ]
             }
